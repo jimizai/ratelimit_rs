5609f44cbda796e8c9fd0711465b3f74d856338f
dyk <359743984@qq.com>
2021-03-27 10:47:32 +0800

    feat: add take_max_duration test && bug fixed

--- src/lib.rs
+++ src/lib.rs
@@ -45,6 +45,7 @@ impl Bucket {
     fn adjust_available_tokens(&mut self, tick: f64) {
         self.latest_tick = Instant::now();
         if self.available_tokens >= self.capacity {
+            self.available_tokens = self.capacity;
             return;
         }
         self.available_tokens += (tick * self.quantum as f64) as u64;
@@ -84,13 +85,13 @@ impl Bucket {
             self.available_tokens = avail as u64;
             return (ZERO_TIME, true);
         }
-        let end_tick = tick + (((self.quantum as i64) - 1 - avail) as f64) / self.quantum as f64;
+        let end_tick = (-avail as f64) / self.quantum as f64;
         let wait_time = (self.fill_interval.as_millis() as f64) * end_tick;
         if wait_time > max_wait.as_millis() as f64 {
             return (ZERO_TIME, false);
         }
-        self.available_tokens = avail as u64;
-        (Duration::from_nanos(wait_time as u64), true)
+        self.available_tokens = -avail as u64;
+        (Duration::from_millis(wait_time as u64), true)
     }
 
     pub fn take_max_duration(&mut self, count: u64, max_wait: Duration) -> Duration {
@@ -98,8 +99,12 @@ impl Bucket {
         wait_time
     }
 
-    pub fn wait_max_duration(&mut self, count: u64, max_wait: Duration) {
-        thread::sleep(self.take_max_duration(count, max_wait))
+    pub fn wait_max_duration(&mut self, count: u64, max_wait: Duration) -> bool {
+        let (sleep_time, ok) = self.take(count, max_wait);
+        if sleep_time.as_millis() > 0 {
+            thread::sleep(sleep_time);
+        }
+        ok
     }
 }
 
@@ -109,7 +114,7 @@ mod tests {
     use std::thread;
     use std::time::Duration;
     #[test]
-    fn it_works() {
+    fn take_avaliable_works() {
         let mut bucket = Bucket::new(Duration::from_secs(3), 100, 100, 100);
         let count = bucket.take_available(100);
         assert_eq!(count, 100);
@@ -124,5 +129,18 @@ mod tests {
         thread::sleep(Duration::from_secs(3));
         let count = bucket.take_available(200);
         assert_eq!(100, count);
+    }
+
+    #[test]
+    fn take_max_duration_works() {
+        let mut bucket = Bucket::new(Duration::from_secs(3), 100, 100, 100);
+        bucket.take_available(100);
+        let time = bucket.take_max_duration(100, Duration::from_secs(4));
+        assert_eq!(time.as_millis(), 3000);
+        let time = bucket.take_max_duration(100, Duration::from_secs(1));
+        assert_eq!(time.as_millis(), 0);
+        thread::sleep(Duration::from_secs(1));
+        let time = bucket.take_max_duration(100, Duration::from_secs(7));
+        assert_eq!(time.as_secs(), 2);
     }
 }
